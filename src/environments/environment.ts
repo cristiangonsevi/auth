a9f150d8c0d29db7888f8f8d3735b95165a5999d
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-12 18:22:07 -0600

    feat: :sparkles: Add google ouath vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  oAuth: {
+    google: {
+      client_id:
+        '377801554737-c6kmgufst864r3de6fk3b37k9b2m3qtr.apps.googleusercontent.com',
+      project_id: 'dogwood-flames-294814',
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_secret: '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',
+      javascript_origins: ['http://localhost:4200'],
+    },
+  },
 };
 
 /*
