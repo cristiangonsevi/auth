1b133a9dd6c33836d9b8b5f46b13eda770896857
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-15 17:27:44 -0600

    feat: :sparkles: Add provider sweet alert service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { SweetAlertService } from './services/sweet-alert.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     AuthModule
   ],
-  providers: [],
+  providers: [SweetAlertService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
