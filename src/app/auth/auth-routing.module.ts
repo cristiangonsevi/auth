9eeb9952f747a3887654edfc63a7847197375a47
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-17 16:41:34 -0600

    feat: :sparkles: Add auth routes

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const router: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(router)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
