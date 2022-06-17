55c338ae74d32d04168b009001349ade4156048f
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-16 18:30:18 -0600

    feat: :sparkles: Add user model

---
+++ src/app/auth/interfaces/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  image: string;
+  authMethod: string;
+  isActive: boolean;
+  createdAt: string;
+}
