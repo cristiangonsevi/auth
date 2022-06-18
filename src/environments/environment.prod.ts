73131c493cdf4120557fe3952ca809b5f0218c02
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-18 13:28:03 -0600

    feat: :sparkles: Add vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    host: 'http://localhost',
+    url: 'http://localhost:3000',
+  },
+  oAuth: {
+    github: {
+      client_id: 'cc92af7bafbb1d436318',
+    },
+    google: {
+      client_id:
+        '435112618636-4h4v3kgdo4hfjj1oqdjkq1ijnpj5ptbo.apps.googleusercontent.com',
+    },
+  },
 };
