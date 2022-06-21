5f00bc9741b03da3ebdcae32f472c4a0551c7a48
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-20 19:27:55 -0600

    feat: :sparkles: Add user logged interface

---
+++ src/app/interfaces/userLoggedIn.interface.ts
@@ -0,0 +1,10 @@
+export interface UserLoggedIn {
+  authMethod: string;
+  createdAt: string;
+  email: string;
+  firstName: string;
+  id: number;
+  image: string;
+  isActive: true;
+  lastName: string;
+}
