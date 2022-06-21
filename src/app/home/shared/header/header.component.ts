e408439c1ecbb2db631cf8450403b08a2b82f83e
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-21 01:03:59 -0600

    feat: :sparkles: Add header design

--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  toggleMenu: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!this._eRef.nativeElement.contains(event.target)) {
+      this.toggleMenu = false;
+    }
   }
+  constructor(private _eRef: ElementRef) {}
 
+  ngOnInit(): void {}
 }
