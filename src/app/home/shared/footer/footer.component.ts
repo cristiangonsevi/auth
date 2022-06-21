ead55b4d344f0c8a88eb4e66fe17d03ae9929931
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-21 01:04:21 -0600

    feat: :sparkles: Add footer design

--- src/app/home/shared/footer/footer.component.ts
+++ src/app/home/shared/footer/footer.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  today = new Date();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
