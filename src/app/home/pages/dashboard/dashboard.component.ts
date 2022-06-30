3849bc905be1d04cb7670d92853d0bd2e2bf90c0
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-30 12:30:39 -0600

    feat: :sparkles: Add current user info

--- src/app/home/pages/dashboard/dashboard.component.ts
+++ src/app/home/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/auth/interfaces/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  currentUser!: User;
+  constructor(private _userService: UserService) {}
 
   ngOnInit(): void {
+    this.currentUser = this._userService.getUserLoggedIn();
   }
-
 }
