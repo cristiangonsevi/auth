55b81962a9749680885f68eab51772380e0b36f5
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-06-30 12:31:14 -0600

    feat: :sparkles: Add call to get current user

--- src/app/home/pages/profile/profile.component.ts
+++ src/app/home/pages/profile/profile.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/auth/interfaces/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  currentUser!: User;
 
-  constructor() { }
+  constructor(private _userService: UserService) {}
 
   ngOnInit(): void {
+    this.currentUser = this._userService.getUserLoggedIn();
   }
-
 }
