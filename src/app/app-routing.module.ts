0f94cdf6016b225a3f38ce6ec9fbfab4dff79f63
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-07-01 09:23:48 -0600

    feat: :sparkles: Add guart to home routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
