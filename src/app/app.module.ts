ee37ef150335f4011d5c4106ce2a89e690a8a0a4
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-07-01 09:24:08 -0600

    feat: :sparkles: Import http and auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './guards/auth.guard';
 
 import { LocalStorageService } from './services/local-storage.service';
 import { SweetAlertService } from './services/sweet-alert.service';
@@ -14,8 +16,9 @@ import { SweetAlertService } from './services/sweet-alert.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [SweetAlertService, LocalStorageService],
+  providers: [SweetAlertService, LocalStorageService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
