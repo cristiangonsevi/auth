fc3d5d451014c1883742636ae5636a74b3e8c9ba
Cristian Gonzalez <albertsevilla1996@gmail.com>
2022-08-13 19:08:18 -0600

    feat: :sparkles: Add user observable - To change when user data is updated

--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/auth/interfaces/user.model';
 import { UserService } from 'src/app/services/user.service';
-import { getImage } from 'src/app/state/profile/profile.reducers';
+import { getImage, getUser } from 'src/app/state/profile/profile.reducers';
 import { environment } from 'src/environments/environment';
 import * as authAction from '../../../state/auth/auth.actions';
 
@@ -18,6 +18,7 @@ export class HeaderComponent implements OnInit {
   toggleMenu: boolean = false;
   host = environment;
   imgUploaded: Observable<string | null> = new Observable();
+  userDataUpdated: Observable<User | null> = new Observable();
   @HostListener('document:click', ['$event'])
   clickout(event: any) {
     if (!this._eRef.nativeElement.contains(event.target)) {
@@ -32,6 +33,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.currentUser = this._userService.getUserLoggedIn();
     this.imgUploaded = this._store.select(getImage)
+    this.userDataUpdated = this._store.select(getUser)
   }
   logout() {
     this._store.dispatch(authAction.LOGOUTREQUESTACTION());
